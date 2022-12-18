ae9669efb086e5e3abeef77bb4cc1a33b8cff609
Mariah Laqua <mariahlaqua@gmail.com>
2022-12-18 14:58:18 +0100

    Remove old routing

--- server.js
+++ server.js
@@ -30,29 +30,6 @@ app.use(cors())
 //in theory if more pages were added, this would send the user along.
 app.use('/', homeRoutes)
 
-//the routes below are from the project before updating to Model-view-architecture
-
-/*
-app.get('/',(request, response)=>{
-   db.collection('books').find().toArray()
-    .then(data => {
-        response.render('index.ejs'/*, {info: data}*//*)
-        console.log(data)
-    })
-    .catch(error => console.error(error))
-})
-
-app.post("/addBook", (request, response) => {
-    db.collection('books').insertOne({bookTitle: request.body.bookTitle,
-    bookAuthor: request.body.bookAuthor, bookDescription: request.body.bookDescription, imgUrl: request.body.imgUrl,
-    descriptionUrl: request.body.descriptionUrl, monthAndYear: request.body.monthAdded, googleBooksId: request.body.googleBooksId})
-    .then(result => {
-        console.log('Book added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-}) */
-
 app.listen(process.env.PORT, ()=>{
     console.log(`The server is running. What joy.`)
 })
